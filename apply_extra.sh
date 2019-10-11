aa37f6783fcb9eb7de4c16105c42501b8d955e8d
Lctrs <jerome@prmntr.me>
2019-10-11 16:27:34 +0200

    Switch to the .deb package
    - Use the embedded icon
    - Use the embedded desktop file

--- apply_extra.sh
+++ apply_extra.sh
@@ -2,5 +2,17 @@
 
 set -e
 
-tar -xf gitkraken.tar.gz
-rm -f gitkraken.tar.gz
+ar x gitkraken.deb
+tar xf data.tar.xz
+
+install -Dm0644 usr/share/gitkraken/gitkraken.png export/share/icons/hicolor/512x512/apps/com.axosoft.GitKraken.png
+
+desktop_file="export/share/applications/com.axosoft.GitKraken.desktop"
+install -Dm0644 usr/share/applications/gitkraken.desktop "$desktop_file"
+desktop-file-edit --set-key="Exec" --set-value="gitkraken %U" "$desktop_file"
+desktop-file-edit --set-key="Icon" --set-value="com.axosoft.GitKraken" "$desktop_file"
+desktop-file-edit --set-key="StartupWMClass" --set-value="gitkraken" "$desktop_file"
+
+mv usr/share/gitkraken gitkraken
+
+rm -rf gitkraken.deb control.tar.gz data.tar.xz debian-binary usr
