2f74e7c9ec8865a9a1a68d736df6a774e58c1923
Lctrs <jerome@prmntr.me>
2020-04-23 12:06:33 +0000

    Ship again icons and desktop file

--- apply_extra.sh
+++ apply_extra.sh
@@ -4,14 +4,6 @@ set -e
 
 ar p gitkraken.deb data.tar.xz | tar -xJf -
 
-install -Dm0644 usr/share/gitkraken/gitkraken.png export/share/icons/hicolor/512x512/apps/com.axosoft.GitKraken.png
-
-desktop_file="export/share/applications/com.axosoft.GitKraken.desktop"
-install -Dm0644 usr/share/applications/gitkraken.desktop "$desktop_file"
-desktop-file-edit --set-key="Exec" --set-value="gitkraken %U" "$desktop_file"
-desktop-file-edit --set-key="Icon" --set-value="com.axosoft.GitKraken" "$desktop_file"
-desktop-file-edit --set-key="StartupWMClass" --set-value="gitkraken" "$desktop_file"
-
 mv usr/share/gitkraken gitkraken
 
 rm -rf gitkraken.deb usr
