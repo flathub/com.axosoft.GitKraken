e74c00fd559ea7cec94d99d69757b9f1f40b87ed
Eonfge <33983090+Eonfge@users.noreply.github.com>
2025-06-25 11:17:06 +0200

    Update message

--- gitkraken.sh
+++ gitkraken.sh
@@ -5,7 +5,7 @@ set -e
 FLAG_FILE="$XDG_CONFIG_HOME/.zenity_please_ask_for_support"
 
 if [ ! -f "$FLAG_FILE" ]; then
-    zenity --info --text='Please, ask Axosoft to officially support Flatpak. With recent upstream changes, this package has become far less reliable. \n\n <a href="https://feedback.gitkraken.com/suggestions/308722/flatpak-installer-on-flathub">Gitkraken Suggestions</a>'
+    zenity --info --text='Please, ask Axosoft to officially support Flatpak. \n\n <a href="https://feedback.gitkraken.com/suggestions/308722/flatpak-installer-on-flathub">Gitkraken Suggestions</a>'
     touch "$FLAG_FILE"
 fi
 
